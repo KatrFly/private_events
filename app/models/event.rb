73e476cb5841093c1f52ab2524f4fad42732958b
KatrFly <katrien.vliegen@gmail.com>
2022-01-06 23:07:58 +0100

    user page now shows a list of past and future events

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
   has_many :invitations
   has_many :attendees, through: :invitations, source: :user
+
+  scope :past, -> { where("date < ?", Date.current) }
+  scope :future, -> { where("date >= ?", Date.current) }
 end
